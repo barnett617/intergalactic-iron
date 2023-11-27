c55bcfaa11bcba5e1c542d5cb841f1bfaad5a3e0
wangxin <wangxin@jwzg.com>
2023-11-27 19:36:58 +0800

    feat: add deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://barnett617.github.io',
+  base: '/intergalactic-iron',
+});
